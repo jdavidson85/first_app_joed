0f9aa9851603e01221c5f82b0f54094077945b55
Joe Davidson <jdavidson@students.mchenry.edu>
2024-02-03 20:46:40 -0600

    Add files via upload

---
+++ first_app/first_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  first_app
+//
+//  Created by Joseph Davidson on 2/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("My First App and I'm not sure where to put my code")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ first_app/first_app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first_app
+//
+//  Created by Joseph Davidson on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
